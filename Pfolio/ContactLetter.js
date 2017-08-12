1278cf56c6dfc029a3f8f83612ee4cc8be1943cc
AnumehaPathak <anumeha.pathak02@gmail.com>
2017-08-12 18:31:50 +0530

    first

---
+++ Pfolio/ContactLetter.js
@@ -0,0 +1,13 @@
+﻿var lApp = angular
+    .module("lModule", [])
+    .controller("lController", function ($scope) {
+        var letter = {
+            name: null,
+            email: null,
+            message: null
+        }
+        $scope.letter = letter;
+        
+    });
+
+   
---
+++ Pfolio/LetsScript.js
@@ -0,0 +1,55 @@
+﻿$(document).ready(function () {
+    $(".slidesec").click(function (e) {
+        var newLink = $(this).attr("href");
+        $("html, body").animate({
+            scrollTop: $(newLink).offset().top - 10
+        }, 1500);
+        e.preventDefault();
+    });
+
+
+    var arr = ["college", "10th", "12th"];
+    jQuery.each(arr, function (i, val) {
+        $("#card-" + val).on("mouseover", function () {
+            $("#DIV3").css("background - attachment", "fixed");
+            $("#DIV3").css("background", "url(" + val + ".jpg)");
+            // $("#DIV3").addClass("div3");
+            $("#card-" + val).removeClass("card");
+            $("#card-" + val).addClass("card-transparent");
+        });
+        $("#card-" + val).on("mouseout", function () {
+            $("#card-" + val).removeClass("card-transparent");
+            $("#card-" + val).addClass("card");
+            //$("#DIV3").removeClass("div3");
+        });
+    });
+
+
+    $("#message").focus(function () {
+        $("#letter").css("visibility", 'visible');
+        $("#letter").animate({
+            height: '400px',
+            width: '250px'
+        });
+    });
+    $("#btn").click(function () {
+        var div = $("#letter");
+        div.animate({ width: '100px' }, "slow");
+        div.animate({ height: '100px' }, "slow");
+        div.animate({ height:'0px' }, "slow");
+        div.animate({ width: '0px' }, "slow", function () {
+            $("#letter").css("visibility", 'hidden');
+        });
+    });
+
+    var arr1 = ["1", "2", "3", "4", "5", "6", "7"];
+    $.each(arr1, function (j, i) {
+        $(document).on("scroll", function () {
+            if ($(document).scrollTop() > $("#skill").offset().top / 2) {
+                $("#arrow-status" + i).addClass("arrow-status");
+                $("#arrow-text" + i).addClass("arrow-text");
+            }
+        });
+    });
+  
+});
